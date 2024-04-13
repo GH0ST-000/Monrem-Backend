a04a22b055e474fc97571ba83affc4d6352e972d
Luka <luka@Lukas-MacBook-Pro.local>
2024-04-13 16:58:03 +0400

    MN-8 Add Connect Admin To Fron-End

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\AdminAuth;
 use App\Http\Controllers\CrawlerController;
+use App\Http\Controllers\LandingPage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/fetch-articles', [CrawlerController::class, 'scrape']);
@@ -13,3 +14,5 @@ Route::post('admin/login',[AdminAuth::class,'login']);
 Route::get('/',function (){
     return view('welcome');
 });
+
+Route::get('/',[LandingPage::class,'index']);
